c1eb04e9ebf28c9719b8c5333f2e6a0a82a580e5
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-08-04 06:18:38 +0000

    CRUD todo

--- forms/src/components/Todo/index.tsx
+++ forms/src/components/Todo/index.tsx
@@ -3,24 +3,42 @@ import './index.css'
 const TODO = () => {
     const [todoInput, setTodoInput] = useState('')
     const [todos, setTodos] = useState<string[]>([])
+    const [editTodo, setEditTodo] = useState<string | null>(null)
     const handleTodoChange = (event: ChangeEvent<HTMLInputElement>) => {
         setTodoInput(event.target.value)
     }
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        // console.log(todoInput)
-        if (todoInput) {
-            setTodos([...todos, todoInput])
-            saveState()
+        if (todoInput.trim()) {
+            saveState([...todos, todoInput])
             setTodoInput('')
         }
     }
 
-    function saveState() {
+    function deleteTodo(todo: string) {
+        const newTodos = todos.filter((t) => t !== todo)
+        // setTodos(newTodos)
+        console.log('newTodos', newTodos)
+        saveState(newTodos)
+    }
+    function saveState(todos: string[]) {
         localStorage.setItem('todos', JSON.stringify(todos))
+        setTodos(todos)
     }
     function loadState() {
         setTodos(JSON.parse(localStorage.getItem('todos') ?? '[]') ?? [])
+    } function editTodoHandler(todo: string) {
+        setEditTodo(todo)
+        setTodoInput(todo)
+    }
+    function handleEditSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        if (editTodo && todoInput.trim()) {
+            const newTodos = todos.map((t) => (t === editTodo ? todoInput : t))
+            saveState(newTodos)
+            setEditTodo(null)
+            setTodoInput('')
+        }
     }
     useEffect(() => {
         loadState()
@@ -28,17 +46,18 @@ const TODO = () => {
     return (
         <section className='todo-section'>
             <h2 className='todo-heading'>Add todo</h2>
-            <form className='todo-form' onSubmit={handleSubmit} >
+            <form className='todo-form' onSubmit={editTodo ? handleEditSubmit : handleSubmit} >
                 <label htmlFor="todo">Todo</label>
                 <input id='todo' type="text" value={todoInput} onChange={handleTodoChange} />
-                <button type="submit">Submit</button>
+                <button type="submit">{editTodo ? 'Edit' : "Submit"}</button>
             </form>
+            {!todos.length && <p>No todos</p>}
             <ul>
-                {todos.map((todo) =>
-                (<li>
+                {todos.map((todo, index) =>
+                (<div key={`${todo}-${index}`} onDoubleClick={() => deleteTodo(todo)}>
                     <span>{todo}</span>
-                    <button>Delete</button>
-                </li>
+                    <button onClick={() => editTodoHandler(todo)}>EDIT</button>
+                </div >
                 )
                 )}
             </ul>
